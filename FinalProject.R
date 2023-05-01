092bd0be0158dc9fd743f6835ad4d2e78ceeec93
cdlesage <cdl23@live.unc.edu>
2023-04-30 23:20:03 -0400

    mapping and other updates

--- FinalProject.R
+++ FinalProject.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(tidycensus)
 library(sf)
 library(ggplot2)
+library(vtable)
 
 ## Loading variables and saving as csv to choose the relevant ones
 acs5_2013 = load_variables(2013, "acs5")
@@ -9,7 +10,7 @@ write_csv(acs5_2013, "acs5_2013.csv")
 
 ## Reading in the spatial data
 blockgroups = read_sf("2010_Census_Block_Groups/2010_Census_Block_Groups.shp")
-
+blockgroups2021 = read_sf("tl_2021_37_bg/tl_2021_37_bg.shp")
 ## Making a list of our variables
 variables=c(
   "TotalPop"="B02001_001",
@@ -44,10 +45,7 @@ acs_2013 = get_acs(
 )
 acs_2013$PercentMinority = ((acs_2013$TotalPopE - acs_2013$WhiteE) / acs_2013$TotalPopE) *100
 acs_2013$Percent25andOverWithBachelorsDegree = (acs_2013$BachelorsDegreeE / acs_2013$TotalPopE) *100
-acs_2013_spatial = merge(blockgroups, # Spatial Data 
-                        acs_2013, #Tabular Data
-                        by.x = "geoid10", # Column in Spatial data used to merge
-                        by.y = "GEOID")
+
 
 
 write_csv(acs_2013, "acs_2013.csv")
@@ -132,10 +130,15 @@ acs_2021 = get_acs(
   survey="acs5",
   output="wide"
 )
+acs_2021$PercentMinority = ((acs_2021$TotalPopE - acs_2021$WhiteE) / acs_2021$TotalPopE) *100
+acs_2021$Percent25andOverWithBachelorsDegree = (acs_2021$BachelorsDegreeE / acs_2021$TotalPopE) *100
+
+
+
 
 ## Identifying at risk neighborhoods
 ## Finding the lowest/highest 100 block groups for each of the important variables
-acs_2013_income = acs_2013[,c("NAME","MedianHouseholdIncomeE")]
+acs_2013_income = acs_2013[,c("NAME","MedianHouseholdIncomeE","GEOID")]
 acs_2013_income= arrange(acs_2013_income, MedianHouseholdIncomeE)
 acs_2013_income= head(acs_2013_income, 100)
 
@@ -148,8 +151,7 @@ acs_2013_education = acs_2013[,c("NAME","Percent25andOverWithBachelorsDegree")]
 acs_2013_education= arrange(acs_2013_education, Percent25andOverWithBachelorsDegree)
 acs_2013_education= head(acs_2013_education, 100)
 
-acs_2013_minority = group_by(acs_2013, NAME) %>%
-  summarize(PercentMinority=mean(PercentMinority))
+acs_2013_minority = acs_2013[,c("NAME","PercentMinority")]
 acs_2013_minority= arrange(acs_2013_minority, desc(PercentMinority))
 acs_2013_minority= head(acs_2013_minority, 100)
 
@@ -158,5 +160,48 @@ join1= merge(acs_2013_income, acs_2013_grossrent, by="NAME")
 join2= merge(join1, acs_2013_education, by="NAME")
 join3= merge(join2, acs_2013_minority, by="NAME")
 
+join3spatial = merge(blockgroups, # Spatial Data 
+                     join3, #Tabular Data
+                     by.x = "geoid10", # Column in Spatial data used to merge
+                     by.y = "GEOID")
 
 
+
+## Summary statistics of the first and last years in the data being used
+summary2013= sumtable(acs_2013)
+summary2021= sumtable(acs_2021)
+
+## Mapping variables in 2013 vs 2021
+## First merging the data with a spatial block group layer
+acs_2013_spatial = merge(blockgroups, # Spatial Data 
+                         acs_2013, #Tabular Data
+                         by.x = "geoid10", # Column in Spatial data used to merge
+                         by.y = "GEOID")
+acs_2021_spatial = merge(blockgroups2021, # Spatial Data 
+                         acs_2021, #Tabular Data
+                         by.x = "GEOID", # Column in Spatial data used to merge
+                         by.y = "GEOID")
+
+## Mapping PercentMinority
+ggplot(acs_2013_spatial) +
+  geom_sf(aes(fill=PercentMinority))
+ggplot(acs_2021_spatial) +
+  geom_sf(aes(fill=PercentMinority))
+
+## Mapping MedianGrossRent
+ggplot(acs_2013_spatial) +
+  geom_sf(aes(fill=MedianGrossRentE))
+ggplot(acs_2021_spatial) +
+  geom_sf(aes(fill=MedianGrossRentE))
+
+## Mapping Percent of Pop 25 and Over with a Bachelors Degree
+ggplot(acs_2013_spatial) +
+  geom_sf(aes(fill=MedianGrossRentE))
+ggplot(acs_2021_spatial) +
+  geom_sf(aes(fill=MedianGrossRentE))
+
+## Mapping Household Income
+ggplot(acs_2013_spatial) +
+  geom_sf(aes(fill=MedianGrossRentE))
+ggplot(acs_2021_spatial) +
+  geom_sf(aes(fill=MedianGrossRentE))
